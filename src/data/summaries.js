b8bd8186b8550fdfeae0bb0fe177f389ee0d45a8
Isaac <isaacbrendel1@gmail.com>
2024-07-31 03:08:56 -0400

    i

---
+++ src/data/summaries.js
@@ -0,0 +1,29 @@
+export const summaries = {
+    1: `According to Russell’s definition, philosophy is the intermediate between science and theology. It asks questions not answerable by science and doesn’t provide dogmatic explanations like theology tends to, and thus philosophy teaches one to grapple with uncertainty without being swayed by comfortable falsehoods. 
+
+Russell in his work sets out to explore the tension of one’s personal philosophy being informed by the events of his day and the events of one’s day being informed by his own personal philosophy, and how this cycle tends to inform the history of philosophy and events that we know as history. 
+
+The early days of philosophy showed the relationship between personal philosophy and civic affairs greatly. Russell points to examples such as Sparta, which had few freedoms from the rule of the state and Athens, which afforded many more freedoms to its people. The latter is known for its great explorations of thought and conjectures about the natural world (because science and philosophy were not distinct in their beginnings), while Sparta lacking the freedom of thoughtful explorations, is know for its warriors and not its thinkers. 
+
+In the beginning of philosophy there wasn’t much space between itself and religion. Religious superstitions informed the behaviors of men, which in turn dictated the events of the time, which in turn informed the philosophies of men. Thus as religions evolved so did the philosophy of the world and of the individual. Athenian philosophers, owing to their freedoms from the state, theorized that one’s soul is responsible for doing right by god as opposed to the state. 
+
+Russell illustrates clearly how early philosophical ideas contributed to the creation of the next generation of ideas (religious, scientific and thus philosophical) and ultimately effecting the ideas and traditions of modern society. `,
+    2: ``,
+    3: ``,
+    4: ``,
+    5: ``,
+    6: ``,
+    7: ``,
+    8: ``,
+    9: ``,
+    10: ``,
+    11: ``,
+    12: ``,
+    13: ``,
+    14: ``,
+    15: ``,
+    16: ``,
+    17: ``,
+    18: ``,
+    19: ``
+  };
